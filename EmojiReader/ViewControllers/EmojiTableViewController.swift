64ef1ef5b84864a6e267fc09879a0b2f7365213f
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-02-14 20:25:50 +0300

    save new emoji in tableview

--- EmojiReader/ViewControllers/EmojiTableViewController.swift
+++ EmojiReader/ViewControllers/EmojiTableViewController.swift
@@ -35,6 +35,28 @@ class EmojiTableViewController: UITableViewController {
         
         navigationItem.leftBarButtonItem = editButtonItem
     }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        guard segue.identifier == "saveSegue" else { return }
+        let sourceVC = segue.source as! NewEmojiTableViewController
+        let emoji = sourceVC.emoji
+        
+        let indexPath = IndexPath(row: objects.count, section: 0)
+        objects.append(emoji)
+        tableView.insertRows(at: [indexPath], with: .fade)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        guard segue.identifier == "editEmoji" else { return }
+        let indexPath = tableView.indexPathForSelectedRow!
+        let emoji = objects[indexPath.row]
+        let navigationVC = segue.destination as! UINavigationController
+        let newEmojiVC = navigationVC.topViewController as! NewEmojiTableViewController
+        newEmojiVC.emoji = emoji
+        newEmojiVC.title = "Edit"
+    }
 
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
--- EmojiReader/ViewControllers/NewEmojiTableViewController.swift
+++ EmojiReader/ViewControllers/NewEmojiTableViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
-
+    
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavourite: false)
     
     @IBOutlet var emojiTextField: UITextField!
     @IBOutlet var nameTextField: UITextField!
@@ -17,10 +18,42 @@ class NewEmojiTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
+        updateSaveButtonState()
     }
-
+    
+    private func updateSaveButtonState() {
+        let emojiText = emojiTextField.text ?? ""
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descriptionTextField.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
+    private func updateUI() {
+        emojiTextField.text = emoji.emoji
+        nameTextField.text = emoji.name
+        descriptionTextField.text = emoji.description
+    }
+    
     @IBAction func textChange(_ sender: Any) {
+        updateSaveButtonState()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        guard segue.identifier == "saveSegue" else { return }
+        let emoji = emojiTextField.text ?? ""
+        let name = nameTextField.text ?? ""
+        let description = descriptionTextField.text ?? ""
+        
+        self.emoji = Emoji(
+            emoji: emoji,
+            name: name,
+            description: description,
+            isFavourite: self.emoji.isFavourite
+        )
     }
     
 }
